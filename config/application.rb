d4f05feb3fdcea9447fc9b407c7f7522e7a49b0c
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-25 17:17:05 -0300

    add rspec factorybot shoulda_matchers rubocop

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module MktPlaceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+    end
   end
 end
