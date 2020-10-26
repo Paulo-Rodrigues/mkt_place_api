0c98d9d46db3cb8888b7331879caa3d76a2428e3
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-26 13:25:46 -0300

    serialized products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,18 +6,18 @@ class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
 
-    render json: @products, status: :ok
+    render json: ProductSerializer.new(@products).serializable_hash, status: :ok
   end
 
   def show
-    render json: @product, status: :ok
+    render json: ProductSerializer.new(@product).serializable_hash, status: :ok
   end
 
   def create
     @product = current_user.products.build(product_params)
 
     if @product.save
-      render json: @product, status: :created
+      render json: ProductSerializer.new(@product).serializable_hash, status: :created
     else
       render json: { errors: @product.errors }, status: :unprocessable_entity
     end
@@ -25,7 +25,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      render json: @product, status: :ok
+      render json: ProductSerializer.new(@product).serializable_hash, status: :ok
     else
       render json: @product.errors, status: :unprocessable_entity
     end
